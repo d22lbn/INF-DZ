aaa42d7225128b0b7e8a38f40c0fa3324390dc46
d22lbn <d22lbn@gmail.com>
2022-03-09 13:56:08 +0300

    Disassembled Dependency injection. Examples via constructor and via setters. An example of how to take values from a file.

--- src/main/java/org/example/test/MusicPlayer.java
+++ src/main/java/org/example/test/MusicPlayer.java
@@ -1,14 +1,68 @@
 package org.example.test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
+
+    private List<Music> musicList;
 
     // инверсия управления
     public MusicPlayer(Music music) {
         this.music = music;
     }
 
+    public MusicPlayer() {
+        musicList = new ArrayList<>();
+    }
+
+    public MusicPlayer(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    public List<Music> getMusicList() {
+        return musicList;
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
     public void playMusic() {
         System.out.println("playing: " + music.getSong());
+    }
+
+    public void playMusics() {
+        System.out.print("Playing: ");
+        for (int i = 0; i < musicList.size(); i++) {
+            System.out.print(musicList.get(i).getSong());
+            if (i < musicList.size() - 1) {
+                System.out.print(", ");
+            }
+        }
     }
 }
--- src/main/java/org/example/test/TestMain.java
+++ src/main/java/org/example/test/TestMain.java
@@ -10,9 +10,20 @@ public class TestMain {
 //        TestBean testBean = context.getBean("testBean", TestBean.class);
 //        System.out.println(testBean.getName());
 
-        Music music = context.getBean("musicBean", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-        musicPlayer.playMusic();
+//        Music music = context.getBean("musicBean", Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+
+//        MusicPlayer musicPlayer = context.getBean("musicPlayerBean", MusicPlayer.class);
+//        musicPlayer.playMusic();
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
+
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayerBeansConstr", MusicPlayer.class);
+        musicPlayer2.playMusics();
+        System.out.println();
+        MusicPlayer musicPlayer3 = context.getBean("musicPlayerBeansSetter", MusicPlayer.class);
+        musicPlayer3.playMusics();
+
 
         context.close();
     }
